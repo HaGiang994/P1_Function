2a808790bd4690ae4e2e1761624d2762679fb707
HaGiang994 <gianglehongha2805@gmail.com>
2016-03-03 16:59:33 +0700

    Add sourcecode
    This is file SumOf2Num

--- SumOf2Num/SumOf2Num/main.m
+++ SumOf2Num/SumOf2Num/main.m
@@ -7,29 +7,14 @@
 //
 
 #import <Foundation/Foundation.h>
-
-@interface SimpleClass : NSObject
-- (NSNumber *)sumA:(NSNumber *)a withB:(NSNumber *)b;
-
-@end
-
-@implementation SimpleClass
-
-- (NSNumber *)sumA:(NSNumber *)a withB:(NSNumber *)b{
-    int num1 = [a intValue];
-    int num2 = [b intValue];
-    int res = num1 + num2;
-    NSNumber *result = [NSNumber numberWithInt:res];
-    return result;
+int sum(int a, int b){
+    NSLog(@"Sum of a and b is: %i",a + b);
+    return 0;
 }
-
-@end
-
 int main(int argc, const char * argv[]) {
-    SimpleClass *simpleClass = [[SimpleClass alloc]init];
-    NSNumber *a = [NSNumber numberWithInt:10];
-    NSNumber *b = [NSNumber numberWithInt:20];
-    NSNumber *result = [simpleClass sumA:a withB:b];
-    NSLog(@"The resulf of sum is: %@",result);
+    @autoreleasepool {
+        int result;
+        result = sum(10, 20);
+    }
     return 0;
 }
