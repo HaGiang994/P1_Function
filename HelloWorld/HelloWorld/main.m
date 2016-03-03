faa89a48c8f3c2f66c52322baf194200827375ba
HaGiang994 <gianglehongha2805@gmail.com>
2016-03-03 16:58:26 +0700

    Add source code
    This is file HelloWorld

--- HelloWorld/HelloWorld/main.m
+++ HelloWorld/HelloWorld/main.m
@@ -8,21 +8,10 @@
 
 #import <Foundation/Foundation.h>
 
-@interface SimpleClass : NSObject
-- (void)simpleMethod;
-@end
-
-@implementation SimpleClass
-
-- (void)simpleMethod{
-    NSLog(@"Hello, World!\n");
+void sayhello(){
+    NSLog(@"Hello, World!");
 }
-
-@end
-
 int main(int argc, const char * argv[]) {
-   //my first program in Objective C
-    SimpleClass *simpleClass = [[SimpleClass alloc]init];
-    [simpleClass simpleMethod];
-    return 0;
+    //my first program in Objective C
+    sayhello();
 }
